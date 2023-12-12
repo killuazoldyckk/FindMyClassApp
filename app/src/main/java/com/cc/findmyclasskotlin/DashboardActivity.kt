1cf8f6923f1d6699e600ab17de5ef16cd6ec0cbf
LorenzoLiu75 <lorenzoliu797@gmail.com>
2023-12-12 10:14:49 +0700

    get user's username and place it to dashboard username text

--- app/src/main/java/com/cc/findmyclasskotlin/DashboardActivity.kt
+++ app/src/main/java/com/cc/findmyclasskotlin/DashboardActivity.kt
@@ -1,9 +1,13 @@
 package com.cc.findmyclasskotlin
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.cc.findmyclasskotlin.databinding.ActivityDashboardBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -11,13 +15,31 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
     private lateinit var dbref: DatabaseReference
+    private lateinit var databaseReference: DatabaseReference
     private lateinit var classRecyclerView: RecyclerView
     private lateinit var roomArrayList: ArrayList<Room>
+    private lateinit var auth: FirebaseAuth
+    private lateinit var user: User
+    private lateinit var uid: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        uid = auth.currentUser?.uid.toString()
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        if (uid.isNotEmpty()) {
+
+            getUserData()
+        }
+
 
         classRecyclerView = findViewById(R.id.class_list)
         classRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -25,6 +47,20 @@ class DashboardActivity : AppCompatActivity() {
 
         roomArrayList = arrayListOf<Room>()
         getClassData()
+    }
+
+    private fun getUserData() {
+
+        databaseReference.child(uid).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                user = snapshot.getValue(User::class.java)!!
+                binding.namaLengkap.text = user.nama
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@DashboardActivity, "Failed to get user profile data", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun getClassData() {

03a2cd9f3b9bcc7fa23bbe9ab81f13f0e3788627
Vio Aprivia Nugraha <vioapriviablah@gmail.com>
2023-12-15 14:34:16 +0700

    Add files via upload

---
+++ admin_findmyclass/jadwal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jadwal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body{
+            text-align: center;
+        }
+        .tambah_jadwal btn a{
+            text-decoration: none;
+        }
+        
+        
+    </style>
+</head>
+<body>
+    <div class="tambah_jadwal mt-3">
+        <h2 class="mb-3">Jadwal</h2>
+        <a href="add_jadwal.php" type="button" class="btn btn-primary">Tambah Jadwal</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
